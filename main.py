0c24e8a455bf86b657f4d857baa18d1ccf39bc13
Ivanova Polina <ivanova.p@mail.ru>
2023-02-01 20:02:11 +0300

    second task

--- main.py
+++ main.py
@@ -2,19 +2,32 @@ import os
 import sys
 
 import requests
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap, QWheelEvent
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from map import Ui_MainWindow
 
 SCREEN_SIZE = [600, 450]
 
 
-class Example(QWidget):
+class StaticMap(QMainWindow, Ui_MainWindow):
+    api_server = "http://static-maps.yandex.ru/1.x/"
+
     def __init__(self):
         super().__init__()
         self.getImage()
         self.initUI()
+        self.setupUi(self)
 
     def getImage(self):
+        self.current_ll = (37.530887, 55.703118)
+        self.current_spn = (0.002, 0.002)
+        self.type = 'map'
+        params = {
+            "ll": ",".join(map(str, self.current_ll)),
+            "spn": ",".join(map(str, self.current_ll)),
+            "l": self.type
+        }
         map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
         response = requests.get(map_request)
 
@@ -29,26 +42,35 @@ class Example(QWidget):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        ## Изображение
         self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+
+    def initUI(self):
+        uic.loadUI('map.ui', self)
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        if event.angleDelta().y() > 0:
+            self.changeMapScale('-')
+        else:
+            self.changeMapScale('+')
+        self.get_Image()
+
+    def changeMapScale(self, eventScaleType: str):
+        current_change = 1
+        if eventScaleType == '-':
+            current_change = current_change
+        new_spn = (self.current_spn[0] + current_change, self.current_spn[1] + current_change)
+        if 0 <= new_spn[0] <= 90 and 0 <= new_spn[1] <= 90:
+            self.current_spn = new_spn
+
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = StaticMap()
     ex.show()
     sys.exit(app.exec())
